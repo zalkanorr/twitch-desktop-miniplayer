297e84484b9e4dd66fb7eae815737b06f54d8b74
Alexandros Papadopoulos <zalkanor@gmail.com>
2019-06-07 04:58:18 +0300

    Add vue router

--- app/src/entry.js
+++ app/src/entry.js
@@ -1,13 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import Zutre from 'zutre';
 
 import "zutre/dist/zutre.css";
 import "video.js/dist/video-js.css";
 
+import App from './App.vue';
+import Home from './components/home.vue';
+import StreamWindow from './components/stream-window.vue';
+
+
+Vue.use(VueRouter);
 Vue.use(Zutre);
+
+const router = new VueRouter({
+  routes: [
+    { 
+      path: '/',
+      component: Home
+    },
+    { 
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/stream-window',
+      component: StreamWindow
+    }
+  ]
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
