c7c0faa84be1cdf5e3e5878e244988f34232ae73
Alexandros Papadopoulos <zalkanor@gmail.com>
2019-06-07 05:05:25 +0300

    Add home component

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 // Basic init
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {ipcMain, app, BrowserWindow} = electron
 
 // Let electron reloads by itself when webpack watches changes in ./app/
 require('electron-reload')(__dirname)
 
 // To avoid being garbage collected
 let mainWindow
-
+var streamWindow = null;
+var stream_data = null;
 app.on('ready', () => {
 
-    let mainWindow = new BrowserWindow({width: 800, height: 600})
+	let mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600
+	})
 
-    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
+	ipcMain.on('streamWindowOpen', function (e, data) {
+		console.log('streamWindowOpen');
+	});
 })
