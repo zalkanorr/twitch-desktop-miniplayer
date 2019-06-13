bab011555d2637b18807a4e8ed54f8a9aa4d133c
Alexandros Papadopoulos <zalkanor@gmail.com>
2019-06-14 02:37:12 +0300

    Add second window instance check

--- main.js
+++ main.js
@@ -16,116 +16,130 @@ var tray = null;
 // The tray menu variable
 var tray_context_menu = null;
 
-app.on('ready', () => {
-	mainWindow = new BrowserWindow({
-		icon: icon_path,
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true
+const isSingleInstance = app.requestSingleInstanceLock();
+
+if (!isSingleInstance) {
+	app.quit()
+} else {
+	app.on('second-instance', (event, commandLine, workingDirectory) => {
+		// Someone tried to run a second instance, we should focus our window.
+		if (mainWindow) {
+			if (mainWindow.isMinimized()) mainWindow.restore()
+			mainWindow.focus()
 		}
 	});
 
-	mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-	initializeTray();
-
-	// streamWindowOpen event is emitted when the render process requests to open a new stream window.
-	// In the event, the data required for the stream window is included and then stored in the stream_data variable.
-	ipcMain.on('streamWindowOpen', function (e, data) {
-		console.log('streamWindowOpen');
-		streamWindow = new BrowserWindow({
+	app.on('ready', () => {
+		mainWindow = new BrowserWindow({
 			icon: icon_path,
-			width: 384,
-			height: 216,
-			toolbar: false,
-			frame: false,
+			width: 800,
+			height: 600,
 			webPreferences: {
 				nodeIntegration: true
 			}
 		});
 
-		// Forcing the window to be ontop
-		streamWindow.setAlwaysOnTop(true, 'floating');
-		streamWindow.setVisibleOnAllWorkspaces(true);
-		streamWindow.setFullScreenable(false);
+		mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-		streamWindow.loadURL(`file://${__dirname}/index.html#/stream-window`);
+		initializeTray();
 
-		// Store data
-		stream_data = data;
-
-		// Make tray menu item visible
-		tray_context_menu.getMenuItemById('close-stream').visible = true;
-	});
-
-	// streamWindowSendMeData event is emmited when the streamWindow is created and is ready to get the data required.
-	ipcMain.on('streamWindowSendMeData', () => {
-		console.log('streamWindowSendMeData');
-		// Send main window to tray
-		mainWindow.hide();
-		// Send the stream data to the main window
-		streamWindow.webContents.send('streamData', stream_data);
-	});
-
-	// streamWindowSendMeData event is emmited when the streamWindow requests to close.
-	ipcMain.on('streamWindowClose', () => {
-		console.log('streamWindowClose');
-		// Close stream window
-		streamWindow.close();
-		// Make tray menu item not visible
-		tray_context_menu.getMenuItemById('close-stream').visible = false;
-		// Send Stop Playing event at main window
-		mainWindow.webContents.send('stopPlaying');
-	});
-
-	function initializeTray() {
-		tray = new Tray(icon_path);
-		tray_context_menu = Menu.buildFromTemplate([
-			{
-				label: 'Open menu',
-				click: function () {
-					mainWindow.show();
+		// streamWindowOpen event is emitted when the render process requests to open a new stream window.
+		// In the event, the data required for the stream window is included and then stored in the stream_data variable.
+		ipcMain.on('streamWindowOpen', function (e, data) {
+			console.log('streamWindowOpen');
+			streamWindow = new BrowserWindow({
+				icon: icon_path,
+				width: 384,
+				height: 216,
+				toolbar: false,
+				frame: false,
+				webPreferences: {
+					nodeIntegration: true
 				}
-			},
-			{
-				label: 'Close stream',
-				id: 'close-stream',
-				visible: false,
-				click: function () {
-					ipcMain.emit('streamWindowClose');
+			});
+
+			// Forcing the window to be ontop
+			streamWindow.setAlwaysOnTop(true, 'floating');
+			streamWindow.setVisibleOnAllWorkspaces(true);
+			streamWindow.setFullScreenable(false);
+
+			streamWindow.loadURL(`file://${__dirname}/index.html#/stream-window`);
+
+			// Store data
+			stream_data = data;
+
+			// Make tray menu item visible
+			tray_context_menu.getMenuItemById('close-stream').visible = true;
+		});
+
+		// streamWindowSendMeData event is emmited when the streamWindow is created and is ready to get the data required.
+		ipcMain.on('streamWindowSendMeData', () => {
+			console.log('streamWindowSendMeData');
+			// Send main window to tray
+			mainWindow.hide();
+			// Send the stream data to the main window
+			streamWindow.webContents.send('streamData', stream_data);
+		});
+
+		// streamWindowSendMeData event is emmited when the streamWindow requests to close.
+		ipcMain.on('streamWindowClose', () => {
+			console.log('streamWindowClose');
+			// Close stream window
+			streamWindow.close();
+			// Make tray menu item not visible
+			tray_context_menu.getMenuItemById('close-stream').visible = false;
+			// Send Stop Playing event at main window
+			mainWindow.webContents.send('stopPlaying');
+		});
+
+		function initializeTray() {
+			tray = new Tray(icon_path);
+			tray_context_menu = Menu.buildFromTemplate([
+				{
+					label: 'Open menu',
+					click: function () {
+						mainWindow.show();
+					}
+				},
+				{
+					label: 'Close stream',
+					id: 'close-stream',
+					visible: false,
+					click: function () {
+						ipcMain.emit('streamWindowClose');
+					}
+				},
+				{
+					label: 'Quit',
+					click: function () {
+						app.exit();
+					}
 				}
-			},
-			{
-				label: 'Quit',
-				click: function () {
-					app.exit();
+			]);
+
+			tray.setToolTip('Twitch Desktop Miniplayer');
+			tray.setContextMenu(tray_context_menu);
+
+			tray.on('click', () => {
+				mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+			});
+
+			mainWindow.on('show', () => {
+				tray.setHighlightMode('always');
+			});
+
+			mainWindow.on('hide', () => {
+				tray.setHighlightMode('always');
+			});
+
+			mainWindow.on('close', function (event) {
+				// Making sure that close event is not emitted by app.quit()
+				if (!app.isQuiting) {
+					event.preventDefault();
+					mainWindow.hide();
 				}
-			}
-		]);
-
-		tray.setToolTip('Twitch Desktop Miniplayer');
-		tray.setContextMenu(tray_context_menu);
-
-		tray.on('click', () => {
-			mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
-		});
-
-		mainWindow.on('show', () => {
-			tray.setHighlightMode('always');
-		});
-
-		mainWindow.on('hide', () => {
-			tray.setHighlightMode('always');
-		});
-
-		mainWindow.on('close', function (event) {
-			// Making sure that close event is not emitted by app.quit()
-			if (!app.isQuiting) {
-				event.preventDefault();
-				mainWindow.hide();
-			}
-			return false;
-		});
-	}
-});
+				return false;
+			});
+		}
+	});
+}
